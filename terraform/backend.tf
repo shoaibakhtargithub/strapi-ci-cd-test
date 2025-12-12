5978d52254d69b1b315ec4283746b91ad81d750c
shoaibmirza@123 <mirzashoaibbeg786@gmail.com>
2025-12-12 15:12:34 +0530

    updated .gitignore and terraform

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "shoaib-terraform-state-bucket"
-    key            = "strapi/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
-}
